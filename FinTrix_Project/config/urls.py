c71e36c2ff77d7b5e395501420f4d83e675b5d7b
adhamtamer874-tech <adhamtamer874@gmail.com>
2026-05-06 14:21:52 +0300

    Rename API paths for transaction endpoints

--- FinTrix_Project/config/urls.py
+++ FinTrix_Project/config/urls.py
@@ -5,8 +5,8 @@ from transactions.views import list_transactions, add_transaction, transactions_
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
-    path('api/', list_transactions, name='list_transactions'),
-    path('api/add/', add_transaction, name='add_transaction'),
+    path('api/', list_transactions, name='api_list_transactions'),
+    path('api/add/', add_transaction, name='api_add_transaction'),
     path('transactions/', transactions_page, name='transactions_page'),
     path("goals/", include("goals.urls")),
     path("budgets/", include("budgets.urls")),
