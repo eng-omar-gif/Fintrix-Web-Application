ea9dacd8d408cbfd4df22bee0458f71ab8dfcc85
adhamtamer874-tech <adhamtamer874@gmail.com>
2026-05-06 13:05:28 +0300

    Add transactions_page to URL imports

--- FinTrix_Project/config/urls.py
+++ FinTrix_Project/config/urls.py
@@ -1,6 +1,6 @@
 ﻿from django.contrib import admin
 from django.urls import path, include
-from transactions.views import list_transactions, add_transaction
+from transactions.views import list_transactions, add_transaction, transactions_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
